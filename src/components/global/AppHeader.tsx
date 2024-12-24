c32478bd25f21da01d0ab11e6f22e48679041709
MD Maruful Islam <maruful@seative.digital>
2024-12-24 17:01:46 +0600

    added login with google and logout functionality

--- src/components/global/AppHeader.tsx
+++ src/components/global/AppHeader.tsx
@@ -7,33 +7,24 @@ type PropTypes = {
 };
 
 const AppHeader = ({ children }: PropTypes) => {
-  const context = useContext(AuthContext);
+  const authContext = useContext(AuthContext);
 
-  // Ensure the context is not undefined (meaning it must be within an AuthProvider)
-  if (!context) {
-    return <div>Loading...</div>; // or handle the undefined case appropriately
+  if (!authContext) {
+    throw new Error("AuthContext must be used within an AuthProvider");
   }
 
-  const { accessInfo } = context;
+  const { loginWithGoogle, logout, user } = authContext;
 
-  const handleAuthentication = () => {
-    if (!accessInfo) {
-      return;
-    }
+  const handleGoogleLogin = () => {
+    loginWithGoogle()
+      .then()
+      .catch((error) => console.log(error));
+  };
 
-    const redirectUrl = window.location.href;
-
-    const state = btoa(redirectUrl);
-
-    const queryParams = new URLSearchParams({
-      accessToken: accessInfo.accessToken || "",
-      refreshToken: accessInfo.refreshToken || "",
-      userVerified: accessInfo.userVerified?.toString() || "null",
-      state,
-    });
-
-    // Open the new window
-    window.open(`http://localhost:3000?${queryParams.toString()}`, "_blank");
+  const handleLogout = () => {
+    logout()
+      .then()
+      .catch((error) => console.log(error));
   };
 
   return (
@@ -48,29 +39,35 @@ const AppHeader = ({ children }: PropTypes) => {
             <Button variant="actionSecondary">Get Support</Button>
           </a>
         </div>
-        <button
-          onClick={handleAuthentication}
-          className="p-1 rounded bg-background-3 text-text-1"
-        >
-          <svg
-            width="16"
-            height="16"
-            viewBox="0 0 16 16"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
+        {user ? (
+          <button onClick={handleLogout} className="text-white">
+            logout
+          </button>
+        ) : (
+          <button
+            onClick={handleGoogleLogin}
+            className="p-1 rounded bg-background-3 text-text-1"
           >
-            <path
-              fill-rule="evenodd"
-              clip-rule="evenodd"
-              d="M8 2C6.34315 2 5 3.34315 5 5C5 6.65685 6.34315 8 8 8C9.65685 8 11 6.65685 11 5C11 3.34315 9.65685 2 8 2ZM6 5C6 3.89543 6.89543 3 8 3C9.10457 3 10 3.89543 10 5C10 6.10457 9.10457 7 8 7C6.89543 7 6 6.10457 6 5Z"
-              fill="#F5F5F5"
-            />
-            <path
-              d="M3 11.5C3 10.6716 3.67157 10 4.5 10H11.5C12.3284 10 13 10.6716 13 11.5V13H14V11.5C14 10.1193 12.8807 9 11.5 9H4.5C3.11929 9 2 10.1193 2 11.5V13H3V11.5Z"
-              fill="#F5F5F5"
-            />
-          </svg>
-        </button>
+            <svg
+              width="16"
+              height="16"
+              viewBox="0 0 16 16"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M8 2C6.34315 2 5 3.34315 5 5C5 6.65685 6.34315 8 8 8C9.65685 8 11 6.65685 11 5C11 3.34315 9.65685 2 8 2ZM6 5C6 3.89543 6.89543 3 8 3C9.10457 3 10 3.89543 10 5C10 6.10457 9.10457 7 8 7C6.89543 7 6 6.10457 6 5Z"
+                fill="#F5F5F5"
+              />
+              <path
+                d="M3 11.5C3 10.6716 3.67157 10 4.5 10H11.5C12.3284 10 13 10.6716 13 11.5V13H14V11.5C14 10.1193 12.8807 9 11.5 9H4.5C3.11929 9 2 10.1193 2 11.5V13H3V11.5Z"
+                fill="#F5F5F5"
+              />
+            </svg>
+          </button>
+        )}
       </div>
     </header>
   );
--- src/providers/AuthProvider.tsx
+++ src/providers/AuthProvider.tsx
@@ -1,11 +1,23 @@
-import { createContext, ReactNode, useState } from "react";
+import { createContext, ReactNode, useEffect, useState } from "react";
+import {
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithPopup,
+  UserCredential,
+  User,
+  signOut,
+} from "firebase/auth";
+import { auth } from "../firebaseConfig";
 
-type User = Record<string, unknown>;
+const googleProvider = new GoogleAuthProvider();
 
 export type AuthContextTypes = {
-  name: string;
   user: User | null;
   setUser: React.Dispatch<React.SetStateAction<User | null>>;
+  loading: boolean;
+  setLoading: React.Dispatch<React.SetStateAction<boolean>>;
+  loginWithGoogle: () => Promise<UserCredential>;
+  logout: () => Promise<void>;
 };
 
 export const AuthContext = createContext<AuthContextTypes | null>(null);
@@ -16,9 +28,35 @@ interface AuthProviderProps {
 
 export default function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<User | null>(null);
-  const name = "adnan";
+  const [loading, setLoading] = useState(true);
 
-  const authData: AuthContextTypes = { user, setUser, name };
+  const loginWithGoogle = (): Promise<UserCredential> => {
+    setLoading(true);
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  const logout = (): Promise<void> => {
+    setLoading(true);
+    return signOut(auth);
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+
+      setLoading(false);
+    });
+    return () => unsubscribe();
+  }, []);
+
+  const authData: AuthContextTypes = {
+    user,
+    setUser,
+    loading,
+    setLoading,
+    loginWithGoogle,
+    logout,
+  };
 
   return (
     <AuthContext.Provider value={authData}>{children}</AuthContext.Provider>
